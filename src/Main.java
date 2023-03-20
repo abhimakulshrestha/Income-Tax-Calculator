ea9f66523b0230fb577541a4e8b2149b9ee41d0d
abhimakulshrestha <abhimakulshrestha2004@gmail.com>
2023-03-20 13:38:14 +0530

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your income in Lakhs per annum");
+        float tax = 0;
+        float income = sc.nextFloat();
+        if (income <= 2.5f) {
+            tax = tax + 0;
+        } else if (income > 2.5f && income <=5f) {
+            tax = tax + 0.05f * (income - 2.5f);
+        } else if (income > 5.0f && income <=10f) {
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.2f * (income - 5.0f);
+        } else if (income > 10f) {
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.05f * (10.0f - 5f);
+            tax = tax + 0.3f * (income - 10.0f);
+
+        }
+        System.out.println("Total tax paid by the employee is:" +tax );
     }
+
 }
+
